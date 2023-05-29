b9490d4dc432b27958dcff1a4d07591da100f61c
shiv8115 <sk811586@gmail.com>
2023-05-29 19:25:32 +0530

    added env file

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,9 @@ require("@nomicfoundation/hardhat-chai-matchers");
 require("hardhat-deploy");
 require("solidity-coverage");
 require('./tasks');
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
-
+const { PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.18", 
   namedAccounts: {
@@ -17,12 +18,16 @@ module.exports = {
   networks: {
     mumbai: {
       url: "https://polygon-mumbai.g.alchemy.com/v2/r1zLtlI4VzABNRCDTlzwkUudARlrlXRV",
-      accounts:
-        ["29cf1b239b8a1c09bb8740673c32839d991e256b87e072bcb1f49b737e959435"],
+      accounts: [PRIVATE_KEY],
     },
     goerli: {
       url: "https://eth-goerli.g.alchemy.com/v2/XV43uAjGjyYn4FEFd202cfufmON3kMYM",
-      accounts: ["29cf1b239b8a1c09bb8740673c32839d991e256b87e072bcb1f49b737e959435"],
-    }
+      accounts: [PRIVATE_KEY],
+    },
+    fuji: {
+      url: `https://api.avax-test.network/ext/bc/C/rpc`,
+      chainId: 43113,
+      accounts: [PRIVATE_KEY],
+    },
   },
 };
